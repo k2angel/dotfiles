b7c891ca30232b86656dfcaa8018715d3264ac61
k2angel <90847045+k2angel@users.noreply.github.com>
2026-03-25 12:36:50 +0900

    enable fcitx5 mozc-ut

--- home.nix
+++ home.nix
@@ -38,6 +38,32 @@
     LANG = "ja_JP.UTF-8";
   };
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc-ut
+      ];
+      settings = {
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "jp";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0".Name = "keyboard-jp";
+          "Groups/0/Items/1".Name = "mozc";
+        };
+        addons = {
+          classicui.globalSection.Theme = "default-dark";
+        };
+      };
+      waylandFrontend = true;
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
