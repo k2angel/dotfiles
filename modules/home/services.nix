74c17c2d3c43b87b702fce411c3639e5da26040f
k2angel <90847045+k2angel@users.noreply.github.com>
2026-03-29 10:53:44 +0900

    move sway.nix/services.* to services.nix

---
+++ modules/home/services.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+{
+  services = {
+    swayidle = {
+      enable = true;
+
+      events = {
+        before-sleep = "${pkgs.swaylock}/bin/swaylock -f -c 000000";
+        lock = "lock";
+      };
+
+      timeouts = [
+        { timeout = 300; command = "${pkgs.swaylock}/bin/swaylock -f -c 000000"; }
+      ];
+    };
+
+    cliphist = {
+      enable = true;
+      extraOptions = [ "max-items 1000" ];
+    };
+
+    mako ={
+      enable = true;
+
+      settings = {
+        background-color = "#282c34";
+        text-color = "#abb2bf";
+        border-color = "#abb2bf";
+        default-timeout = 5000;
+        "urgency=low" = { border-color = "#abb2bf"; };
+        "urgency=normal" = { border-color = "#abb2bf"; };
+        "urgency=high" = { border-color = "#abb2bf"; };
+      };
+    };
+  };
+}
--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -73,35 +73,4 @@
       }];
     };
   };
-
-  services.swayidle = {
-    enable = true;
-    events = {
-      before-sleep = "${pkgs.swaylock}/bin/swaylock -f -c 000000";
-      lock = "lock";
-    };
-    timeouts = [
-      { timeout = 300; command = "${pkgs.swaylock}/bin/swaylock -f -c 000000"; }
-    ];
-  };
-
-  services.cliphist = {
-    enable = true;
-    extraOptions = [
-      "max-items 1000"
-    ];
-  };
-
-  services.mako ={
-    enable = true;
-    settings = {
-      background-color = "#282c34";
-      text-color = "#abb2bf";
-      border-color = "#abb2bf";
-      default-timeout = 5000;
-      "urgency=low" = { border-color = "#abb2bf"; };
-      "urgency=normal" = { border-color = "#abb2bf"; };
-      "urgency=high" = { border-color = "#abb2bf"; };
-    };
-  };
 }
