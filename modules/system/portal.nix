826c7b6182e5817033b9f8f5fa7b9c2d601f5ca5
k2angel <90847045+k2angel@users.noreply.github.com>
2026-03-29 10:43:42 +0900

    refactor portal

--- modules/system/portal.nix
+++ modules/system/portal.nix
@@ -4,14 +4,14 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
+
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       xdg-desktop-portal-termfilechooser
     ];
+
     config = {
-      common = {
-        default = [ "gtk" ];
-      };
+      common = { default = [ "gtk" ]; };
       sway = {
         "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
       };
