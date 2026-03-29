4a951211327070e66ceea16a8da869f533812e49
k2angel <90847045+k2angel@users.noreply.github.com>
2026-03-29 23:50:51 +0900

    change termfilechooser PATH

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -12,7 +12,7 @@
     cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
     default_dir=$HOME
     env=TERMCMD='foot -T "terminal filechooser" -a "termfilechooser"'
-    env=PATH="$PATH:/run/current-system/sw/bin:/etc/profiles/per-user/${username}/bin"
+    env=PATH="$PATH:/run/current-system/sw/bin"
     open_mode=suggested
     save_mode=last
   '';
