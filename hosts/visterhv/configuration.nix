54387227a8c2ecd8812e1e27b97f26d79cca6273
k2angel <90847045+k2angel@users.noreply.github.com>
2026-03-30 20:29:31 +0900

    enable hosts/visterhv custom edid

--- hosts/visterhv/configuration.nix
+++ hosts/visterhv/configuration.nix
@@ -9,15 +9,21 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.kernelParams = [
-    "video=DP-2:d"
-    "video=HDMI-A-1:e"
-  ];
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
-  };
+  boot = {
+    loader.systemd-boot.enable = lib.mkForce false;
+    initrd.extraFiles."/lib/firmware/edid/lg.bin".source = ./edid/lg.bin;
+
+    kernelParams = [
+      "drm.edid_firmware=DP-2:edid/lg.bin"
+      "video=DP-2:d"
+      "video=HDMI-A-1:e"
+    ];
+
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/var/lib/sbctl";
+    };
+  }
 
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
