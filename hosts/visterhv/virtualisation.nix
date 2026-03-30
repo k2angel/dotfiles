3b0863bdd5b3f9e0db41ebeab893e05cbc7fad4b
k2angel <90847045+k2angel@users.noreply.github.com>
2026-03-30 20:30:37 +0900

    enable oci-containers epoupon/lms

--- hosts/visterhv/virtualisation.nix
+++ hosts/visterhv/virtualisation.nix
@@ -9,6 +9,23 @@
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
     };
+
+    oci-containers = {
+      backend = "podman";
+
+      containers = {
+        lms = {
+          image = "docker.io/epoupon/lms";
+          autoStart = true;
+          extraOptions = [ "--pull=always" ];
+          ports = [ "5082:5082" ];
+          volumes = [
+            "/var/lib/lms-data:/var/lms:rw"
+            "/mnt/pirate/Music/Library:/music:ro"
+          ];
+        };
+      };
+    };
   };
 
   # users.users.${username}.extraGroups = [ "podman" ];
