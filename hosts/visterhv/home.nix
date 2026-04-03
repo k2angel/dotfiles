5353b4281a03bbd40664ee2915d35694f584dbdf
k2angel <90847045+k2angel@users.noreply.github.com>
2026-04-03 18:30:34 +0900

    visterhv: add sway.extraConfig

--- hosts/visterhv/home.nix
+++ hosts/visterhv/home.nix
@@ -15,19 +15,29 @@
         homeDirectory = "/home/${username}";
         stateVersion = "25.11";
       };
-      wayland.windowManager.sway.config = {
-        output = {
-          "HDMI-A-1" = {
-            enable = "";
-            res = "1920x1080";
-            pos = "0 0";
-          };
-          "DP-2" = {
-            disable = "";
-            res = "1920x1080@119.993Hz";
-            pos = "1920 0";
+
+      wayland.windowManager.sway = {
+        config = {
+          output = {
+            "HDMI-A-1" = {
+              enable = "";
+              res = "1920x1080";
+              pos = "0 0";
+            };
+            "DP-2" = {
+              disable = "";
+              res = "1920x1080@119.993Hz";
+              pos = "1920 0";
+            };
           };
         };
+
+        extraConfig = let
+          modifier = config.wayland.windowManager.sway.config.modifier;
+        in ''
+          bindsym ${modifier}+g output DP-2 enable; output HDMI-A-1 disable
+          bindsym ${modifier}+Shift+g output DP-2 disable; output HDMI-A-1 enable
+        '';
       };
 
       programs = {
