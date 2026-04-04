965ac77bd77aa506d3ede64bc9ea2940e89321de
k2angel <90847045+k2angel@users.noreply.github.com>
2026-04-04 09:21:26 +0900

    themes: enable home.pointerCursor

--- modules/home/themes.nix
+++ modules/home/themes.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 
 {
+  home.pointerCursor = {
+    enable = true;
+    package = pkgs.adwaita-icon-theme;
+    name = "Adwaita";
+    size = 24;
+    gtk.enable = true;
+    sway.enable = true;
+    x11.enable = true;
+  };
+
   gtk = {
     enable = true;
     colorScheme = "dark";
