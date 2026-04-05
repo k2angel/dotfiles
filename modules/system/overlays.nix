24eeb47634c627b309deed0b9189c6a85a214d55
k2angel <90847045+k2angel@users.noreply.github.com>
2026-04-06 02:37:44 +0900

    wmenu: changed to use nix-colors

--- modules/system/overlays.nix
+++ modules/system/overlays.nix
@@ -1,14 +1,18 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
-{
-  nixpkgs.overlays = [
+let
+  nix-colors = inputs.nix-colors;
+  colorScheme = nix-colors.colorSchemes.onedark;
+in {
+
+  nixpkgs.overlays = with colorScheme.palette; [
     (self: super: {
       wmenu = super.symlinkJoin {
         name = "wmenu-wrapped";
         paths = [ super.wmenu ];
         nativeBuildInputs = [ self.makeWrapper ];
         postBuild = ''
-          WMENU_FLAGS="-f 'JetBrains Mono NL Regular 11' -N '#282c34' -n '#abb2bf' -M '#61afef' -m '#1e2127' -S '#61afef' -s '#1e2127'"
+          WMENU_FLAGS="-f 'JetBrains Mono NL Regular 11' -N '#${base00}' -n '#${base05}' -M '#${base0D}' -m '#${base00}' -S '#${base0D}' -s '#${base00}'"
           wrapProgram $out/bin/wmenu --add-flags "$WMENU_FLAGS"
           wrapProgram $out/bin/wmenu-run --add-flags "$WMENU_FLAGS"
         '';
