242a66d53013b1f21236b4485fed999a9dc7f0ea
k2angel <90847045+k2angel@users.noreply.github.com>
2026-04-06 02:40:37 +0900

    security: enable polkit

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -1,14 +1,20 @@
 { pkgs, ... }:
 
 {
-  security.sudo.enable = false;
-  security.doas = {
-    enable = true;
-    extraRules = [{
-      groups = [ "wheel" ];
-      keepEnv = true;
-      persist = true;
-    }];
+  security = {
+    polkit.enable = true;
+    sudo.enable = false;
+
+    doas = {
+      enable = true;
+
+      extraRules = [{
+        groups = [ "wheel" ];
+        keepEnv = true;
+        persist = true;
+      }];
+    };
   };
+
   environment.systemPackages = [ pkgs.doas-sudo-shim ];
 }
