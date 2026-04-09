9e6b73fc54586a0a64ba5fac7c45a61d449a1a47
k2angel <90847045+k2angel@users.noreply.github.com>
2026-04-09 17:43:19 +0900

    fix(zsh): LS_COLORS

--- modules/home/zsh/init.zsh
+++ modules/home/zsh/init.zsh
@@ -3,7 +3,7 @@ export PROMPT="[%n@%m %1~]%(#.#.$) "
 
 # completion
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
 
