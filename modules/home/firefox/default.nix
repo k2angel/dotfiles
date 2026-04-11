1640f801e8c4db96558255a5d12c3a9ca6af94bb
k2angel <90847045+k2angel@users.noreply.github.com>
2026-04-11 09:29:15 +0900

    feat(firefox): remove ExtensionSettings and 3rdparty.Extensions

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./betterfox.nix ];
@@ -9,97 +9,6 @@
 
     policies = {
       DisableTelemetry = true;
-      ExtensionSettings = let
-        moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
-      in {
-        "*".installation_mode = "blocked";
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          install_url = moz "bitwarden-password-manager";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "{5cce4ab5-3d47-41b9-af5e-8203eea05245}" = {
-          install_url = moz "control-panel-for-twitter";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "addon@darkreader.org" = {
-          install_url = moz "darkreader";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "{506e023c-7f2b-40a3-8066-bc5deb40aebe}" = {
-          install_url = moz "gesturefy";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "{242af0bb-db11-4734-b7a0-61cb8a9b20fb}" = {
-          install_url = moz "malwarebytes";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
-          install_url = moz "vimium-ff";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "uBlock0@raymondhill.net" = {
-          install_url = moz "ublock-origin";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "@ublacklist" = {
-          install_url = moz "ublacklist";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "{aecec67f-0d10-4fa7-b7c7-609a2db280cf}" = {
-          install_url = moz "violentmonkey";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-        "{d148819b-332d-4519-bfc3-679e49d27112}" = {
-          install_url = moz "one-dark-scheme";
-          installation_mode = "force_installed";
-          updates_disabled = true;
-        };
-      };
-      "3rdparty".Extensions = {
-        "uBlock0@raymondhill.net".adminSettings = {
-          userSettings = rec {
-            importedLists = [
-              "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
-              "https://yuki2718.github.io/adblock2/japanese/jpf-plus.txt"
-            ];
-            externalLists = lib.concatStringsSep "\n" importedLists;
-          };
-          selectedFilterLists = [
-            "user-filters"
-            "ublock-filters"
-            "ublock-badware"
-            "ublock-privacy"
-            "ublock-quick-fixes"
-            "ublock-unbreak"
-            "easylist"
-            "easyprivacy"
-            "adguard-spyware-url"
-            "urlhaus-1"
-            "plowe-0"
-            "fanboy-cookiemonster"
-            "ublock-cookies-easylist"
-            "fanboy-social"
-            "fanboy-ai-suggestions"
-            "easylist-chat"
-            "easylist-newsletters"
-            "easylist-notifications"
-            "easylist-annoyances"
-            "ublock-annoyances"
-            "JPN-1"
-            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
-            "https://yuki2718.github.io/adblock2/japanese/jpf-plus.txt"
-          ];
-        };
-      };
     };
 
     profiles.default = {
