378f1f13cbcb9454f24d2b897ef00d8f5ad65072
k2angel <90847045+k2angel@users.noreply.github.com>
2026-05-14 03:31:44 +0900

    fix(visterhv): change sway.config.keybindings use lib.mkOptionDefault

--- hosts/visterhv/home.nix
+++ hosts/visterhv/home.nix
@@ -1,14 +1,24 @@
-{ lib, ... }:
+{ lib, config, ... }:
 
 {
   wayland.windowManager.sway = {
     config = {
+      keybindings =
+        let
+          modifier = config.wayland.windowManager.sway.config.modifier;
+        in
+        lib.mkOptionDefault {
+          "${modifier}+g" = "output DP-2 enable; output HDMI-A-1 disable";
+          "${modifier}+Shift+g" = "output DP-2 disable; output HDMI-A-1 enable";
+        };
+
       output = {
         "HDMI-A-1" = {
           enable = "";
           res = "1920x1080";
           pos = "0 0";
         };
+
         "DP-2" = {
           disable = "";
           res = "1920x1080@119.993Hz";
@@ -16,15 +26,6 @@
         };
       };
     };
-
-    extraConfig =
-      let
-        modifier = "Mod4";
-      in
-      ''
-        bindsym ${modifier}+g output DP-2 enable; output HDMI-A-1 disable
-        bindsym ${modifier}+Shift+g output DP-2 disable; output HDMI-A-1 enable
-      '';
   };
 
   programs = {
