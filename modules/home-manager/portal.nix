6d29c5cebf55acb5e9b144cd1a649d1f8a505083
k2angel <90847045+k2angel@users.noreply.github.com>
2026-05-15 10:44:02 +0900

    feat(home-manager): add xdg-desktop-portal-wlr,termfilechooser, systemd.user.settings.Manager

--- modules/home-manager/portal.nix
+++ modules/home-manager/portal.nix
@@ -1,11 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   xdg.portal = {
     enable = true;
 
     extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
+      xdg-desktop-portal-termfilechooser
     ];
 
     config = {
@@ -20,6 +22,13 @@
         ];
         "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
       };
+    };
+  };
+
+  systemd.user.settings = {
+    Manager.ManagerEnvironment = {
+      # https://discourse.nixos.org/t/configuring-xdg-desktop-portal-with-home-manager-on-ubuntu-hyprland-via-nixgl/65287/6
+      XDG_DATA_DIRS = "/usr/local/share:/usr/share:${config.home.profileDirectory}/share:/nix/var/nix/profiles/default/share";
     };
   };
 
