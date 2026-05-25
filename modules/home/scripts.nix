df3bf96cc56e0778005a8146027a51447e572431
k2angel <90847045+k2angel@users.noreply.github.com>
2026-05-25 13:31:52 +0900

    feat(scripts): add flock in bemenu-cliphist, wmenu-powermenu

--- modules/home/scripts.nix
+++ modules/home/scripts.nix
@@ -2,6 +2,13 @@
 
 {
   bemenu-cliphist = pkgs.writeScriptBin "bemenu-cliphist" ''
+    LOCKFILE="/tmp/bemenu-cliphist.lock"
+    exec 9>"$LOCKFILE"
+    if ! flock -n 9; then
+      echo "Script is already running." >&2
+        exit 1
+        fi
+
     export BEMENU_BACKEND=curses
     result=$(${pkgs.cliphist}/bin/cliphist list | ${pkgs.bemenu}/bin/bemenu -p cliphist)
 
@@ -29,6 +36,13 @@
   '';
 
   wmenu-powermenu = pkgs.writeShellScriptBin "wmenu-powermenu" ''
+    LOCKFILE="/tmp/wmenu-powermenu.lock"
+    exec 9>"$LOCKFILE"
+    if ! flock -n 9; then
+      echo "Script is already running." >&2
+        exit 1
+        fi
+
     options="Lock\nLogout\nReboot\nShutdown\nSuspend"
     chosen=$(echo -e "$options" | ${pkgs.wmenu}/bin/wmenu -p "System" -l 5)
 
