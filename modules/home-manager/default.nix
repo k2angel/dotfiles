44160b1386c741c4d77c6313db0709322923be61
k2angel <90847045+k2angel@users.noreply.github.com>
2026-05-26 12:52:36 +0900

    feat(home-manager): add imports ../home, remove imports ../home/*
    disable programs firefox, vesktop

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,23 +1,6 @@
 {
   imports = [
-    ../home/nixvim
-    ../home/zsh
-    ../home/beets.nix
-    ../home/btop.nix
-    ../home/fcitx5.nix
-    ../home/foot.nix
-    ../home/git.nix
-    ../home/home.nix
-    ../home/i3status.nix
-    ../home/overlays.nix
-    ../home/packages.nix
-    ../home/programs.nix
-    ../home/yazi.nix
-    ../home/services.nix
-    ../home/sway.nix
-    ../home/themes.nix
-    ../home/uwsm.nix
-    ../home/xdg.nix
+    ../home
 
     ./overrides.nix
     ./portal.nix
--- modules/home-manager/overrides.nix
+++ modules/home-manager/overrides.nix
@@ -7,8 +7,10 @@
 
 {
   programs = {
+    firefox.enable = lib.mkForce false;
     mpv.enable = lib.mkForce false;
     swaylock.package = null;
+    vesktop.enable = lib.mkForce false;
     zsh.shellAliases = {
       nob = lib.mkForce "${pkgs.nh}/bin/nh home build --diff always";
       nos = lib.mkForce "${pkgs.nh}/bin/nh home switch --diff always";
