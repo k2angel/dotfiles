90dbd9c9c01a132b3e084751b03c288c5e74de0f
k2angel <90847045+k2angel@users.noreply.github.com>
2026-06-29 18:54:43 +0900

    feat(archlinux/home): add services.pipewire

--- hosts/archlinux/home.nix
+++ hosts/archlinux/home.nix
@@ -5,6 +5,48 @@
     beets.settings.directory = "/mnt/pirate/Music/Library";
   };
 
+  services = {
+    pipewire = {
+      enable = true;
+
+      clientConfigs = {
+        routing = {
+          "stream.rules" = [
+            {
+              actions = {
+                update-props = {
+                  "target.object" = "alsa_output.usb-ASUSTeK_XONAR_SOUND_CARD-01.analog-stereo";
+                };
+              };
+
+              matches = [
+                { "node.name" = "bmsw-stream"; }
+              ];
+            }
+          ];
+        };
+      };
+
+      pulseConfigs = {
+        routing = {
+          "pulse.rules" = [
+            {
+              actions = {
+                update-props = {
+                  "target.object" = "alsa_output.pci-0000_00_1f.3.analog-stereo";
+                };
+              };
+
+              matches = [
+                { "application.process.binary" = "firefox"; }
+              ];
+            }
+          ];
+        };
+      };
+    };
+  };
+
   home.packages = with pkgs; [
     mcomix
     payload-dumper-go
