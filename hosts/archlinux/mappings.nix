4d5e57024144a257d1abe44a75facc9dbe7ac41a
k2angel <90847045+k2angel@users.noreply.github.com>
2026-07-05 14:59:39 +0900

    feat(archlinux/mappings): add CX True Wireless

--- hosts/archlinux/mappings.nix
+++ hosts/archlinux/mappings.nix
@@ -2,4 +2,5 @@
   "alsa_output.pci-0000_00_1f.3.analog-stereo" = "ALC897 Analog";
   "alsa_output.pci-0000_01_00.1.hdmi-stereo" = "Acer H223HQ";
   "alsa_output.usb-ASUSTeK_XONAR_SOUND_CARD-00.analog-stereo" = "XONAR SOUND CARD";
+  "bluez_output.80_C3_BA_03_C0_BC.1" = "CX True Wireless";
 }
