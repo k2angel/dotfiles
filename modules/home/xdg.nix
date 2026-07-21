eebd6d6c80aa36c9fe56061b8bbd12d64bc384bf
k2angel <90847045+k2angel@users.noreply.github.com>
2026-07-21 14:47:37 +0900

    chore(home/xdg): remove termfilechooser/config TERMCMD -T option and quotation

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -15,7 +15,7 @@
     [filechooser]
     cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
     default_dir=$HOME
-    env=TERMCMD='${pkgs.foot}/bin/foot -T "terminal filechooser" -a "termfilechooser"'
+    env=TERMCMD=${pkgs.foot}/bin/foot -a termfilechooser
     env=PATH="$PATH:${pkgs.gnused}/bin:${pkgs.yazi}/bin"
     open_mode=suggested
     save_mode=last
