b174c2ce3c5d5f631b8edc1edf1179c36b343f16
k2angel <90847045+k2angel@users.noreply.github.com>
2026-09-19 12:19:41 +0900

    sf-pro: 7.0.7 -> 20.0d5e4

--- pkgs/by-name/sf-pro/package.nix
+++ pkgs/by-name/sf-pro/package.nix
@@ -1,23 +1,22 @@
 {
-  stdenv,
+  stdenvNoCC,
   fetchurl,
   p7zip,
 }:
 
-stdenv.mkDerivation {
+stdenvNoCC.mkDerivation {
   pname = "sf-pro";
-  version = "7.0.7";
+  version = "22.0d5e4";
   nativeBuildInputs = [ p7zip ];
 
   src = fetchurl {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-    hash = "sha256-qQlPDem3idc1RO5Q/FKgiE1Kn3/PYt5Sl04yBPOnSmI=";
+    hash = "sha256-loqzuLH5LC2K9h6waA9cIiTE541ZuYa/AEUCp/wBKRg=";
   };
 
   unpackPhase = ''
-    7z e $src "SFProFonts/SF Pro Fonts.pkg"
-    7z e "SF Pro Fonts.pkg"
-    7z e Payload\~ "./Library/Fonts/*"
+    7z e $src
+    7z e -tcpio Payload\~ "./Library/Fonts/*"
   '';
 
   installPhase = ''
