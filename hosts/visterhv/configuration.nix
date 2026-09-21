cbf1735dfc3e0014a46feb3cade9b8d3a84a2312
k2angel <90847045+k2angel@users.noreply.github.com>
2026-09-21 17:36:51 +0900

    fix(configuration.nix, users.nix): update authorized keys for user
    The authorized keys for the specified user have been updated. The keys are now managed in a centralized location within the `configuration.nix` file, ensuring consistency and ease of maintenance across different modules.

--- hosts/visterhv/configuration.nix
+++ hosts/visterhv/configuration.nix
@@ -83,4 +83,8 @@
       };
     };
   };
+
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINy5U5i0X7MxWivscSC289DyUil96Gbdekwfei56ckZ1 u0_a468"
+  ];
 }
--- modules/system/users.nix
+++ modules/system/users.nix
@@ -9,9 +9,5 @@
       "wheel"
       "networkmanager"
     ];
-
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINy5U5i0X7MxWivscSC289DyUil96Gbdekwfei56ckZ1 u0_a468"
-    ];
   };
 }
