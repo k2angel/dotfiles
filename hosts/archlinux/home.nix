9e022b92358d60f20b6cd2fdbfe9df767b42e2fd
k2angel <90847045+k2angel@users.noreply.github.com>
2026-09-21 18:03:36 +0900

    refactor(home.nix): add conditional package for nix-serve-ng on non-NixOS systems The home.packages list now includes the nix-serve-ng package conditionally, only if the system is not NixOS. This ensures that nix-serve-ng is only installed on systems where it is supported and necessary, avoiding potential conflicts or unnecessary installations.

--- hosts/archlinux/home.nix
+++ hosts/archlinux/home.nix
@@ -60,25 +60,30 @@ in
     config.services.mako.package
   ];
 
-  home.packages = with pkgs; [
-    dos2unix
-    ipsw
-    mcomix
-    mkvtoolnix-cli
-    n-m3u8dl-re
-    opencommit
-    payload-dumper-go
-    pipe-rename
-    razer-cli
-    savepagenow
-    sox
-    slsk-batchdl
-    tdl
-    twitch-dl
-    unar
-    wireguard-tools
-    yay
-    xq-xml
-    xnviewmp
-  ];
+  home.packages =
+    with pkgs;
+    [
+      dos2unix
+      ipsw
+      mcomix
+      mkvtoolnix-cli
+      n-m3u8dl-re
+      opencommit
+      payload-dumper-go
+      pipe-rename
+      razer-cli
+      savepagenow
+      sox
+      slsk-batchdl
+      tdl
+      twitch-dl
+      unar
+      wireguard-tools
+      yay
+      xq-xml
+      xnviewmp
+    ]
+    ++ lib.optionals (!isNixOS) [
+      nix-serve-ng
+    ];
 }
